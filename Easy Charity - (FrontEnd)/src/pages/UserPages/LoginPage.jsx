b091fdbe77396535e491779cf9ed5b51f1982889
hespinoza-2018194 <hespinoza-2018194@kinal.edu.gt>
2023-05-26 17:36:58 -0600

    Se añadieron las vistas de Regitrar e Iniciar sesión

--- Easy Charity - (FrontEnd)/src/pages/UserPages/LoginPage.jsx
+++ Easy Charity - (FrontEnd)/src/pages/UserPages/LoginPage.jsx
@@ -1,10 +1,70 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import './Login.css'
 
 export const LoginPage = () => {
   return (
       
       <>
-        <h1>Login Page</h1>
+        <section className="h-100 gradient-form">
+                <div className="container py-5 h-100">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col-xl-10">
+                        <div className="card rounded-3 text-black">
+                        <div className="row g-0">
+                            <div className="col-lg-6">
+                            <div className="card-body p-md-5 mx-md-4">
+
+                                <div className="text-center">
+                                <img src="https://camo.githubusercontent.com/09fe474f27e7ee825551f10be0684371cd5ffc58b047419066947f4f0e0074c7/68747470733a2f2f692e696d6775722e636f6d2f784138523075572e706e67"
+                                    style={{width: "185px"}} alt="logo"/>
+                                <h4 className="mt-1 mb-5 pb-1">Easy Charity</h4>
+                                </div>
+
+                                <form>
+                                <p>Agrege sus datos</p>
+
+                                <div className="form-outline mb-4">
+                                    <input type="email" id="form2Example11" className="form-control"
+                                    placeholder="Coloque su usuario" />
+                                    <label className="form-label" htmlFor="form2Example11">Usuario</label>
+                                </div>
+
+                                <div className="form-outline mb-4">
+                                    <input placeholder="Coloque su contraseña" type="password" id="form2Example22" className="form-control" />
+                                    <label className="form-label" htmlFor="form2Example22">Contraseña</label>
+                                </div>
+
+                                <div className="text-center pt-1 mb-5 pb-1">
+                                    <button className="btn text-center btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
+                                    in</button>
+                                </div>
+
+                                <div className="d-flex align-items-center justify-content-center pb-4">
+                                    <p className="mb-0 me-2">¿No tienes una cuenta?</p>
+                                    <Link to=''>
+                                    <button type="button" className="btn btn-outline-danger">Registrarse</button>
+                                    </Link>
+                                </div>
+
+                                </form>
+
+                            </div>
+                            </div>
+                            <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
+                            <div className="text-white px-3 py-4 p-md-5 mx-md-4">
+                                <h4 className="mb-4">We are more than just a company</h4>
+                                <p className="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </section>
       </>
 
   )
--- Easy Charity - (FrontEnd)/src/pages/UserPages/RegisterPage.jsx
+++ Easy Charity - (FrontEnd)/src/pages/UserPages/RegisterPage.jsx
@@ -1,7 +1,64 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import './Register.css'
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <>
+        <section className="">
+                <div className="container py-5 ">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col">
+                        <div className="card card-registration my-4">
+                        <div className="row g-0">
+                            <div className="col-xl-6 d-none d-xl-block ">
+                            <img id="kinal" src={kinal}
+                                alt="Sample photo" className="img-fluid"/>
+                            </div>
+                            <div className="col-xl-6">
+                            <div className="card-body p-md-5 text-black">
+                                <h3 className="mb-5 text-uppercase">Registrarse</h3>
+
+                                <div className="row">
+                                <div className="col-md-6 mb-4">
+                                    <div className="form-outline">
+                                    <input placeholder="Nombre" type="text" id="form3Example1m" className="form-control form-control-lg" />
+                                    <label className="form-label" htmlFor="form3Example1m">Nombres</label>
+                                    </div>
+                                </div>
+                                <div className="col-md-6 mb-4">
+                                    <div className="form-outline">
+                                    <input placeholder="Apellidos" type="text" id="form3Example1n" className="form-control form-control-lg" />
+                                    <label className="form-label" htmlFor="form3Example1n">Apellidos</label>
+                                    </div>
+                                </div>
+                                </div>
+
+                                <div className="form-outline mb-4">
+                                <input placeholder="Correo" type="text" id="form3Example9" className="form-control form-control-lg" />
+                                <label className="form-label" htmlFor="form3Example9">Correo</label>
+                                </div>
+
+                                <div className="form-outline mb-4">
+                                <input placeholder="Contraseña" type="password" id="form3Example8" className="form-control form-control-lg" />
+                                <label className="form-label" htmlFor="form3Example8">Contraseña</label>
+                                </div>
+
+                                <div className="text-center pt-3">
+                                <button type="button" className="btn btn-light btn-lg">Cancelar</button>
+                                <Link to='/'>
+                                <button type="button" className="btn btn-danger btn-lg ms-2">Registrarse</button>
+                                </Link>
+                                </div>
+
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+         </section>
+        </>
   )
 }
