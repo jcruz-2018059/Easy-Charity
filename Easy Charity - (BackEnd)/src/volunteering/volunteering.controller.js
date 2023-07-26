103fdebd79d370bf7319aa08b37ed87ea89f5b59
jgomez-2018112 <jgomez-2018112@kinal.edu.gt>
2023-07-26 03:47:29 -0600

    Se agregarpn las funciones de Agregar, Buscar y Cancelar en Voluntariado. Tambien se actualizo la coleccion de thunder y se arreglaron errores en las rutas de volutariado

--- Easy Charity - (BackEnd)/src/volunteering/volunteering.controller.js
+++ Easy Charity - (BackEnd)/src/volunteering/volunteering.controller.js
@@ -24,12 +24,58 @@ exports.addVolunteering  = async(req,res)=>{
         let proyect = await Proyect.findOne({_id: data.proyect});
         if(!proyect) return res.status(404).send({message: 'No se encontro el proyecto'});
         
+        let params = {
+            dpi: data.dpi,
+            age: data.age,
+            skills: data.skills,
+            state: true,
+            description: data.description,
+            user: data.user,
+            proyect: data.proyect
+        };
 
-        let volunteering = new Volunteering(data);
+        let volunteering = new Volunteering(params);
         await volunteering.save();
         return res.send({message: 'Se a ha agregado satisfactoriamente su peticion de volunariado', volunteering});
     }catch(err){
         console.error(err);
         return res.status(500).send({message: 'Error al agregarse un voluntario', error: err.message});
+    }
+}
+
+
+exports.getVolunteering = async(req,res)=>{
+    try{
+        let volunteering = await Volunteering.find().populate('user').populate('proyect')
+        return res.send(volunteering)
+    }catch(err){
+        console.error(err);
+        return res.status(500).send({message: 'Error al obtener los voluntariados'});
+    }
+}
+
+exports.cancel = async(req,res)=>{
+    try{
+        let volunteeringId = req.params.id;
+        let volunteering = await Volunteering.findOne({_id: volunteeringId});
+
+        //validar que exista el voluntariado
+        if(!volunteering) return res.status(404).send({message: 'Voluntario no encontrado'});
+
+        let params = {
+            state : false
+        }
+
+        let updateVolunteering = await Volunteering.findOneAndUpdate(
+            {_id: volunteering},
+            params,
+            {new: true},
+        );
+        if(!updateVolunteering) return res.status(404).send({message: 'No se pudo actualizar el estado'});
+
+        return res.send({message: 'Voluntariado Cancelado'})
+    }catch(err){
+        console.error(err);
+        return res.status(500).send
     }
 }
--- Easy Charity - (BackEnd)/src/volunteering/volunteering.routes.js
+++ Easy Charity - (BackEnd)/src/volunteering/volunteering.routes.js
@@ -3,8 +3,12 @@
 const volunteeringController = require('./volunteering.controller');
 const express = require('express');
 const api = express.Router();
+const {ensureAuth, isClient} = require('../../services/authenticated');
 
 api.get('/', volunteeringController.test);
+api.post('/add', [ensureAuth, isClient], volunteeringController.addVolunteering);
+api.get('/get', volunteeringController.getVolunteering);
+api.put('/cancelVol/:id', [ensureAuth, isClient], volunteeringController.cancel);
 
 
 
